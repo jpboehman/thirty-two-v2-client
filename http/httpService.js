fc055c52dc6a19f48814b2becf351408ccf0b91d
Jackson Boehman <jpboehman@jacksons-mbp.mynetworksettings.com>
2023-06-14 19:40:55 -0400

    new env variable add

--- http/httpService.js
+++ http/httpService.js
@@ -1,12 +1,13 @@
-import axios from 'axios';
+import axios from "axios";
 
 axios.defaults.baseURL =
-    // `${process.env.REACT_APP_API_URL}` || 'http://localhost:8080/api/';
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080/api/";
 
-    'http://localhost:8080/api/';
+// 'http://localhost:8080/api/';
 
 let user;
-if (typeof window !== 'undefined') user = JSON.parse(window.localStorage.getItem('persist:root'))?.user;
+if (typeof window !== "undefined")
+  user = JSON.parse(window.localStorage.getItem("persist:root"))?.user;
 // const user = JSON.parse(localStorage?.getItem('persist:root'))?.user;
 const data = user && JSON.parse(user).data?.payload;
 const TOKEN = data?.accessToken;
@@ -14,5 +15,5 @@ const TOKEN = data?.accessToken;
 export const generalRequest = axios.create({});
 
 export const userRequest = axios.create({
-    headers: { 'x-access-token': TOKEN, },
+  headers: { "x-access-token": TOKEN },
 });
