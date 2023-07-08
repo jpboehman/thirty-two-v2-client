4b4904f953d858f5a766b31a974c7ab372a8bb02
Jackson Boehman <jpboehman@jacksons-mbp.mynetworksettings.com>
2023-07-08 16:21:41 -0400

    default exports

---
+++ pages/pages/league-players-page/ncaa-d1-womens.js
@@ -0,0 +1,102 @@
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import styles from "@/styles/PageTitle.module.css";
+import MaterialReactTable from "material-react-table";
+import TableContainer from "@mui/material/TableContainer";
+import Paper from "@mui/material/Paper";
+import NcaaTeamsOverview from "@/components/StatOverviews/ncaaTeamsOverview";
+import { useSelector } from "react-redux";
+import chosenSeason from "common/seasonOptions";
+import useApi from "hooks/useApi";
+
+import InfoIcon from "@mui/icons-material/Info";
+import ReusableBanner from "@/components/Banners/ReusableBanner";
+
+const columns = [
+  { accessorKey: "Player", header: "Player Name" },
+  { accessorKey: "Team", header: "Team" },
+  { accessorKey: "Season Grade", header: "Season Grade" },
+  { accessorKey: "G", header: "G" },
+  { accessorKey: "WCr %", header: "WCr %" },
+  { accessorKey: "WCr/GP", header: "WCr/GP" },
+  { accessorKey: "MVPr", header: "MVPr" },
+  { accessorKey: "MIN", header: "MIN" },
+  { accessorKey: "PTS", header: "PTS" },
+  { accessorKey: "FGM", header: "FGM" },
+  { accessorKey: "FGA", header: "FGA" },
+  { accessorKey: "3FM", header: "3FM" },
+  { accessorKey: "3FA", header: "3FA" },
+  { accessorKey: "2FM", header: "2FM" },
+  { accessorKey: "2FA", header: "2FA" },
+  { accessorKey: "FTM", header: "FTM" },
+  { accessorKey: "FTA", header: "FTA" },
+  { accessorKey: "OREB", header: "OREB" },
+  { accessorKey: "DREB", header: "DREB" },
+  { accessorKey: "REB", header: "REB" },
+  { accessorKey: "AST", header: "AST" },
+  { accessorKey: "STL", header: "STL" },
+  { accessorKey: "BLK", header: "BLK" },
+  { accessorKey: "TO", header: "TO" },
+  { accessorKey: "PF", header: "PF" },
+];
+
+const NcaaD1MensLeaguePlayers = () => {
+  const [ncaaD1MensLeaguePlayers, setNcaaD1MensLeaguePlayers] = useState([]);
+  const router = useRouter();
+  const currentUser = useSelector((state) => state.currentUser?.payload);
+  const [selectedPlayerId, setSelectedPlayerId] = useState(null);
+
+  const { data, isError, errorMessage } = useApi(
+    "/ncaa-d1-mens-league-players",
+    500
+  );
+  useEffect(() => {
+    if (data?.ncaaPlayerLeague)
+      setNcaaD1MensLeaguePlayers(data.ncaaPlayerLeague);
+  }, [data]);
+
+  const handleRowClick = (row) => {
+    const { _id } = row.original;
+    setSelectedPlayerId(_id);
+    const pathname = `/pages/player-page/${_id}`;
+    window.location.pathname = pathname;
+  };
+
+  return (
+    <>
+      <div className={styles.pageTitle}>
+        <h1>NCAA League Players</h1>
+        <ul>
+          <li>
+            <Link href="/">Dashboard</Link>
+          </li>
+        </ul>
+      </div>
+      {ncaaD1MensLeaguePlayers && (
+        <>
+          <TableContainer
+            component={Paper}
+            sx={{
+              boxShadow: "none",
+            }}
+          >
+            <MaterialReactTable
+              columns={columns}
+              data={ncaaD1MensLeaguePlayers}
+              enableColumnOrdering
+              muiTableBodyRowProps={({ row }) => ({
+                onClick: () => handleRowClick(row),
+                sx: {
+                  cursor: "pointer",
+                },
+              })}
+            />
+          </TableContainer>
+        </>
+      )}
+    </>
+  );
+};
+
+export default NcaaD1MensLeaguePlayers;
---
+++ pages/pages/player-page/nba.js
@@ -0,0 +1,104 @@
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import styles from "@/styles/PageTitle.module.css";
+import MaterialReactTable from "material-react-table";
+import TableContainer from "@mui/material/TableContainer";
+import Paper from "@mui/material/Paper";
+import { useSelector } from "react-redux";
+import chosenSeason from "common/seasonOptions";
+import useApi from "hooks/useApi";
+
+import InfoIcon from "@mui/icons-material/Info";
+
+// For player page, not leveraging table to show data, but a profile component instead
+const columns = [
+  { accessorKey: "Player", header: "Player Name" },
+  { accessorKey: "Team", header: "Team" },
+  { accessorKey: "Season Grade", header: "Season Grade" },
+  { accessorKey: "WCr %", header: "WCr %" },
+  { accessorKey: "WCr/GP", header: "WCr/GP" },
+  { accessorKey: "MVPr", header: "MVPr" },
+  { accessorKey: "MIN", header: "MIN" },
+  { accessorKey: "PTS", header: "PTS" },
+  { accessorKey: "FGM", header: "FGM" },
+  { accessorKey: "FGA", header: "FGA" },
+  { accessorKey: "3FM", header: "3FM" },
+  { accessorKey: "3FA", header: "3FA" },
+  { accessorKey: "2FM", header: "2FM" },
+  { accessorKey: "2FA", header: "2FA" },
+  { accessorKey: "FTM", header: "FTM" },
+  { accessorKey: "FTA", header: "FTA" },
+  { accessorKey: "OREB", header: "OREB" },
+  { accessorKey: "DREB", header: "DREB" },
+  { accessorKey: "REB", header: "REB" },
+  { accessorKey: "AST", header: "AST" },
+  { accessorKey: "STL", header: "STL" },
+  { accessorKey: "BLK", header: "BLK" },
+  { accessorKey: "TO", header: "TO" },
+  { accessorKey: "PF", header: "PF" },
+];
+
+const NcaaD1MensTeamRoster = () => {
+  const [ncaaD1MensPlayer, setNcaaD1MensPlayer] = useState([]);
+  const router = useRouter();
+  const currentUser = useSelector((state) => state.currentUser?.payload);
+  const [selectedSeason, setSelectedSeason] = useState([
+    chosenSeason,
+    currentUser,
+  ]);
+  const [selectedPlayerId, setSelectedPlayerId] = useState(null);
+
+  const { id } = router.query;
+
+  // TODO: Update route
+  const { data, isError, errorMessage } = useApi(
+    `/ncaa-d1-mens-player/${id}`,
+    500
+  );
+  useEffect(() => {
+    if (data?.ncaaPlayer) setNcaaD1MensPlayer(data.ncaaPlayer);
+  }, [data]);
+
+  const handleRowClick = (row) => {
+    const { _id } = row.original;
+    setSelectedPlayerId(_id);
+    const pathname = `/pages/player-page/${_id}}`;
+    window.location.pathname = pathname;
+  };
+
+  console.log(ncaaD1MensPlayer);
+
+  return (
+    <>
+      <div className={styles.pageTitle}>
+        <h1>NCAA Roster</h1>
+        <ul>
+          <li>
+            <Link href="/">Dashboard</Link>
+          </li>
+        </ul>
+      </div>
+      {/* <TableContainer
+        component={Paper}
+        sx={{
+          boxShadow: "none",
+        }}
+      >
+        <MaterialReactTable
+          columns={columns}
+          data={ncaaD1MensTeamRosterData}
+          enableColumnOrdering
+          muiTableBodyRowProps={({ row }) => ({
+            onClick: () => handleRowClick(row),
+            sx: {
+              cursor: "pointer",
+            },
+          })}
+        />
+      </TableContainer> */}
+    </>
+  );
+};
+
+export default NcaaD1MensTeamRoster;
